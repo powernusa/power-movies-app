5d21bf5f451b3341706ecc4f17b61382760cc265
powernusa <asoelist1551@yahoo.co.id>
2017-04-07 14:39:49 +0700

    feat: Added menu

--- app/src/main/java/com/powernusa/andy/powermovies/MovieListActivity.java
+++ app/src/main/java/com/powernusa/andy/powermovies/MovieListActivity.java
@@ -2,6 +2,10 @@ package com.powernusa.andy.powermovies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MovieListActivity extends AppCompatActivity {
 
@@ -10,5 +14,34 @@ public class MovieListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_list);
         new FetchMoviesTask().execute();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.sort_by_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.sort_by_most_popular:
+                Toast.makeText(getApplicationContext(),"Most Popular",Toast.LENGTH_SHORT).show();
+                item.setChecked(true);
+                break;
+            case R.id.sort_by_top_rated:
+                Toast.makeText(getApplicationContext(),"Top Rated",Toast.LENGTH_SHORT).show();
+                item.setChecked(true);
+                break;
+            case R.id.sort_by_favorites:
+                Toast.makeText(getApplicationContext(),"Favorites",Toast.LENGTH_SHORT).show();
+                item.setChecked(true);
+                break;
+
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/powernusa/andy/powermovies/utility/Constants.java
+++ app/src/main/java/com/powernusa/andy/powermovies/utility/Constants.java
@@ -7,7 +7,7 @@ package com.powernusa.andy.powermovies.utility;
 public class Constants {
     /* FetchMoviesTask */
     public static final String BASE_URL = "http://api.themoviedb.org/";
-    //public static final String API_KEY = "b41491fab3366590c929b8668512cb0";
+    public static final String API_KEY = "b41491fab3366590c929b8668512cb03";
     public static final String MOST_POPULAR = "popular";
     public static final String TOP_RATED = "top_rated";
     public static final String FAVORITES = "favorites";
